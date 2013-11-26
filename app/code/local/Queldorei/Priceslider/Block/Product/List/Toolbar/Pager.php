dbc363e06189b4cfebd826e9ea356c89e743e9a0
xuannq <xuannq@smartosc.com>
2013-11-26 16:16:44 +0700

    setup

--- app/code/local/Queldorei/Priceslider/Block/Product/List/Toolbar/Pager.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Queldorei_Priceslider_Block_Product_List_Toolbar_Pager extends Mage_Page_Block_Html_Pager
-{
-}
--- app/code/local/Queldorei/ShopperSettings/sql/shoppersettings_setup/mysql4-upgrade-1.3.0-1.4.0.php
+++
@@ -1,9 +0,0 @@
-<?php
-$installer = $this;
-/* @var $installer Mage_Catalog_Model_Resource_Eav_Mysql4_Setup */
-$installer->startSetup();
-$installer->setConfigData('shoppersettings/appearance/timeline', '#322c29');
-$installer->setConfigData('shoppersettings/design/search_field', '0');
-$installer->setConfigData('shoppersettings/design/below_logo', '0');
-$installer->setConfigData('shoppersettings/navigation/use_wide_navigation', '0');
-$installer->endSetup();
